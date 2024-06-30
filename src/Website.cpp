9a47f72db58da0823210fe738ab653dbcdbfd767
Nathaniel Wright <nathan@nathanieljwright.com>
2024-06-30 08:25:27 -0700

    Storing keys and values via POST

--- src/Website.cpp
+++ src/Website.cpp
@@ -1,12 +1,27 @@
 #include <crow.h>
+#include <sstream>
+#include "KVStore.hpp"
 using namespace crow;
+using namespace std;
+using namespace N8;
 
 int main(int argc, char* argv[]) {
   SimpleApp app;
+  KVStore store("webiste.db");
   
-  CROW_ROUTE(app, "/")([]() {
-    return "Hello world";
-  });
+  CROW_ROUTE(app, "/<string>").methods(HTTPMethod::POST)
+    ([&store](const request& req, std::string key) {
+      store.Put(key, req.body);
+
+      stringstream ss;
+      ss << "Stored " << key << ": " << req.body;
+      return ss.str();
+    });
+
+  CROW_ROUTE(app, "/<string>").methods(HTTPMethod::GET)
+    ([&store](std::string key) {
+      return store.Get(key);
+    });
 
   app.port(18080).multithreaded().run();
   return 0;
