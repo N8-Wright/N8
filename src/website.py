8ff73f412ed09b293bdc9a5ab2f2ddb822959bf1
Nathaniel Wright <nathan@nathanieljwright.com>
2024-07-14 14:46:20 -0700

    Added some templates

--- src/website.py
+++ src/website.py
@@ -1,8 +1,14 @@
-﻿from typing import Annotated
-from fastapi import Depends, FastAPI
+﻿from datetime import datetime
+from typing import Annotated
+import uuid
+from fastapi import Depends, FastAPI, Request
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from authentication import require_auth
+from blog_post import BlogPost
 from settings import Settings
+from mistletoe import markdown
 from blog_posts import BlogPosts
 
 g_settings = Settings()
@@ -10,15 +16,35 @@ posts = BlogPosts("blogs.db")
 app = FastAPI()
 security = HTTPBasic()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates/")
+
 @app.get("/")
-def read_root():
+def read_root(request: Request):
     id = posts.create_post("Testing 1234")
     posts.update_post(id, "12345")
     post = posts.get_post(id)
-    
-    return {"Hello": "World"}
+    rendered = markdown("# Hello")
+
+    return templates.TemplateResponse(
+        request=request, name="basic_page.html", context={"body": rendered}
+    )
 
 @app.get("/admin/posts")
-def read_admin_page(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+def read_admin_pages(request: Request, credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     require_auth(g_settings, credentials)
-    return credentials.username
+    posts = [BlogPost(uuid.uuid4(), datetime.now(), datetime.now(), "Testing")]
+
+    return templates.TemplateResponse(
+        request=request, name="admin_posts.html", context={"posts": posts}
+    )
+
+@app.get("/admin/edit/{id}")
+def read_admin_post(id: uuid.UUID, request: Request, credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    require_auth(g_settings, credentials)
+    posts = [BlogPost(uuid.uuid4(), datetime.now(), datetime.now(), "Testing")]
+
+    rendered = markdown("# Hello")
+    return templates.TemplateResponse(
+        request=request, name="basic_page.html", context={"body": rendered}
+    )
