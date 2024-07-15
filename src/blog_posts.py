5a4b3ca5b1c1265a9a2c2e470276612e6e735e79
Nathaniel Wright <nathan@nathanieljwright.com>
2024-07-15 08:42:26 -0700

    Using contextmanager

--- src/blog_posts.py
+++ src/blog_posts.py
@@ -3,60 +3,53 @@ from datetime import datetime
 from time import mktime
 from uuid import uuid4, UUID
 from blog_post import BlogPost
+from sqlite_helpers import connect_with_wal, connection, transaction
 
 class BlogPosts:
     def __init__(self, db_path):
         self.db_path = db_path
-        db_connection = sqlite3.connect(db_path, autocommit=False)
-        cursor = db_connection.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS posts(id BLOB PRIMARY KEY, created_date INTEGER, modified_date INTEGER, content TEXT, name TEXT)")
-        db_connection.close()
+        with connection(db_path) as db_connection:     
+            cursor = db_connection.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS posts(id BLOB PRIMARY KEY, created_date INTEGER, modified_date INTEGER, content TEXT, name TEXT)")
     
     def create_post(self, name: str, content: str) -> UUID:
-        db_connection = sqlite3.connect(self.db_pat, autocommit=False)
-        cursor = db_connection.cursor()
-        
         now = datetime.now()
         unix_now = mktime(now.timetuple())
-
         new_id = uuid4()
-        cursor.execute("INSERT INTO posts VALUES(?, ?, ?, ?, ?)", (new_id.bytes, unix_now, unix_now, content, name))
-        db_connection.commit()
-        db_connection.close()
+        with connection(self.db_path) as db_connection:
+            with transaction(db_connection) as cursor:
+                cursor.execute("INSERT INTO posts VALUES(?, ?, ?, ?, ?)", (new_id.bytes, unix_now, unix_now, content, name))
+
         return new_id
     
     def update_post(self, id: UUID, name: str, content: str) -> None:
-        db_connection = sqlite3.connect(self.db_path, autocommit=False)
-        cursor = db_connection.cursor()
-    
         now = datetime.now()
         unix_now = mktime(now.timetuple())
-        cursor.execute("UPDATE posts SET modified_date=? WHERE id=?", (unix_now, id.bytes, ))
-        cursor.execute("UPDATE posts SET content=? WHERE id=?", (content, id.bytes, ))
-        cursor.execute("UPDATE posts SET name=? WHERE id=?", (name, id.bytes, ))
-        db_connection.commit()
-        db_connection.close()
+        with connection(self.db_path) as db_connection:
+            with transaction(db_connection) as cursor:
+                cursor.execute("UPDATE posts SET modified_date=? WHERE id=?", (unix_now, id.bytes, ))
+                cursor.execute("UPDATE posts SET content=? WHERE id=?", (content, id.bytes, ))
+                cursor.execute("UPDATE posts SET name=? WHERE id=?", (name, id.bytes, ))
 
     def get_post(self, id: UUID) -> BlogPost | None:
-        db_connection = sqlite3.connect(self.db_path, autocommit=False)
-        cursor = db_connection.cursor()
-        post = cursor.execute("SELECT * FROM posts WHERE id=?", (id.bytes, )).fetchone()
-        db_connection.close()
-        if post != None:
-            return BlogPost(id, datetime.fromtimestamp(post[1]), datetime.fromtimestamp(post[2]), post[3], post[4])
-        
+        with connection(self.db_path) as db_connection:
+            cursor = db_connection.cursor()
+            post = cursor.execute("SELECT * FROM posts WHERE id=?", (id.bytes, )).fetchone()
+            if post != None:
+                return BlogPost(id, datetime.fromtimestamp(post[1]), datetime.fromtimestamp(post[2]), post[3], post[4])
+            
         return None
     
     def get_posts(self) -> list[BlogPost]:
-        db_connection = sqlite3.connect(self.db_path, autocommit=False)
-        cursor = db_connection.cursor()
-        res = cursor.execute("SELECT * FROM posts")
+        posts = []
+        with connection(self.db_path) as db_connection:
+            cursor = db_connection.cursor()
+            res = cursor.execute("SELECT * FROM posts")
+            posts = res.fetchall()
         
-        posts = res.fetchall()
         result = []
         for post in posts:
             result.append(BlogPost(UUID(bytes=post[0]), datetime.fromtimestamp(post[1]), datetime.fromtimestamp(post[2]), post[3], post[4]))
         
-        db_connection.close()
         return result
         
---
+++ src/sqlite_helpers.py
@@ -0,0 +1,29 @@
+﻿from contextlib import contextmanager
+from sqlite3 import Connection, Cursor
+import sqlite3
+from typing import Generator
+
+
+def connect_with_wal(database: str) -> Connection:
+    conn = sqlite3.connect(database)
+    conn.execute("PRAGMA journal_mode=WAL")
+    conn.execute("PRAGMA temp_store=2")
+    return conn
+
+@contextmanager
+def connection(database: str) -> Generator[Connection, Connection, Connection]:
+    conn = connect_with_wal(database)
+    try:
+        yield conn
+    finally:
+        conn.close()
+        
+@contextmanager
+def transaction(conn: Connection) -> Generator[Cursor, Cursor, Cursor]:
+    try:
+        yield conn.cursor()
+    except:
+        conn.rollback()
+        raise
+    else:
+        conn.commit()
