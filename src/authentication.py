a0f098c3d6b72d968475af453fccab02023d23a3
Nathaniel Wright <nathan@nathanieljwright.com>
2024-07-17 18:00:53 -0700

    Added license

--- src/authentication.py
+++ src/authentication.py
@@ -1,4 +1,19 @@
-﻿import secrets
+﻿# N8, An amalgamation of personal code
+# Copyright (C) 2024 Nathaniel Wright
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import secrets
 from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
--- src/sqlite_helpers.py
+++ src/sqlite_helpers.py
@@ -1,4 +1,19 @@
-﻿from contextlib import contextmanager
+﻿# N8, An amalgamation of personal code
+# Copyright (C) 2024 Nathaniel Wright
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from contextlib import contextmanager
 from sqlite3 import Connection, Cursor
 import sqlite3
 from typing import Generator
