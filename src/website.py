388c01d7633f41910ac10c0736c53bc24d4a3e3e
Nathaniel Wright <nathan@nathanieljwright.com>
2024-07-21 13:17:30 -0700

    Small fixes

--- src/website.py
+++ src/website.py
@@ -37,8 +37,6 @@ security = HTTPBasic()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
-
 @pass_context
 def urlx_for(context: dict, name: str, **path_params: Any, ) -> str:
     request: Request = context['request']
@@ -46,7 +44,6 @@ def urlx_for(context: dict, name: str, **path_params: Any, ) -> str:
     if scheme := request.headers.get('x-forwarded-proto'):
         return http_url.replace(scheme=scheme)
     return http_url
-
 
 templates = Jinja2Templates(directory="templates/")
 templates.env.globals['url_for'] = urlx_for
