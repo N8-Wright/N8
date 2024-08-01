10baac3efaac3762b59f555060ee46a40dc32708
Nathaniel Wright <nathan@nathanieljwright.com>
2024-07-31 17:18:11 -0700

    Minify css

--- Website/build.sh
+++ Website/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-npx tailwindcss -i ./src/static/input.css -o ./src/static/output.css
+npx tailwindcss -i ./src/static/input.css -o ./src/static/output.css --minify
 pipenv requirements > requirements.txt
 docker build . -t n8website
